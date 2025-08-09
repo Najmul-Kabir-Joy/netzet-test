7600288a2ebdbb20d35ad798a3d5ff305142924f
JoJo Joy <najmulkabir178@gmail.com>
2025-08-09 23:54:09 +0600

    nz-4 rendering navbar on page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
+'use client';
+
+import Navbar from '@/components/ui/Navbar/Navbar';
 import PromotionBar from '@/components/ui/PromotionBar/PromotionBar';
 
 export default function Home() {
   return (
-    <main>
+    <main className="w-full min-h-screen overflow-hidden">
       <PromotionBar />
+      <div className="max-w-[var(--container-xl)] mx-auto">
+        <Navbar />
+      </div>
     </main>
   );
 }
