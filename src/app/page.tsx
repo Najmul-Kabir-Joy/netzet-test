513e7147a8c0152923c56a265eb76e767ea42f1c
JoJo Joy <najmulkabir178@gmail.com>
2025-08-10 11:20:05 +0600

    nz-5 rendering  hero section on Home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import Hero from '@/components/ui/Hero/Hero';
 import Navbar from '@/components/ui/Navbar/Navbar';
 import PromotionBar from '@/components/ui/PromotionBar/PromotionBar';
 
@@ -9,6 +10,7 @@ export default function Home() {
       <PromotionBar />
       <div className="max-w-[var(--container-xl)] mx-auto">
         <Navbar />
+        <Hero />
       </div>
     </main>
   );
